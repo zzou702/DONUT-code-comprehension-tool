93cf4d6f657093f5ab382d1fe640eea18d20d30e
ochu761 <75078332+AoMedium@users.noreply.github.com>
2023-06-12 21:35:40 +1200

    initial project setup

---
+++ backend/api/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+// import topics from "./topics.js";
+
+const router = express.Router();
+
+// router.use("/topics", topics);
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import * as url from "url";
+
+import api from "./api/index.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Setup JSON parsing for request body
+app.use(express.json());
+
+// Enable CORS for all routes from any origin
+app.use(cors());
+
+// Setup our routes.
+app.use("/", api);
+
+// Make the "public" folder available statically
+app.use(
+  express.static(
+    path.join(url.fileURLToPath(new URL(".", import.meta.url)), "public")
+  )
+);
+
+app.listen(port, () => console.log(`App server listening on port ${port}!`));

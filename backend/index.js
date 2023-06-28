791dacb691f2b46e8cb0544293d816bfd20afc15
zzou702 <zzou702@aucklanduni.ac.nz>
2023-06-28 17:29:46 +1200

    Added necessary dependencies for jwtwebtoken, mongoose and bcrypt

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,27 @@ import cors from "cors";
 import path from "path";
 import * as url from "url";
 import api from "./api/index.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 3000;
+// const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://zmoi:seven7777777@donut.gnfo9so.mongodb.net/myDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+  });
 
 // Setup JSON parsing for request body
 app.use(express.json());

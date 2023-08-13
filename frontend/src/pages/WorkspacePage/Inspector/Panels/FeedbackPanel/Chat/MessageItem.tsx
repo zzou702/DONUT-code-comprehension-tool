adafd7f9d50814a280c35bf48769e0e61b74b333
ochu761 <75078332+AoMedium@users.noreply.github.com>
2023-08-13 22:53:21 +1200

    fix styling + render msg as markdown

--- frontend/src/pages/WorkspacePage/Inspector/Panels/FeedbackPanel/Chat/MessageItem.tsx
+++ frontend/src/pages/WorkspacePage/Inspector/Panels/FeedbackPanel/Chat/MessageItem.tsx
@@ -2,6 +2,7 @@ import { Stack, Typography } from "@mui/material";
 import Panel from "../../../../../../components/Panel";
 import { spacing } from "../../../../SharedStyles";
 import Message from "../../../../../../models/Message";
+import ReactMarkdown from "react-markdown";
 
 const receivedMessageStyle = {
   marginRight: "auto",
@@ -11,10 +12,13 @@ const selfMessageStyle = {
 };
 
 const receivedPanelStyle = {
+  background: "#655DC3",
+  color: "white",
   borderTopLeftRadius: 0,
 };
 
 const selfPanelStyle = {
+  background: "#ddd",
   borderBottomRightRadius: 0,
 };
 
@@ -27,19 +31,27 @@ export default function MessageItem({ message }: Props) {
   const panelStyle = message.isUser ? selfPanelStyle : receivedPanelStyle;
 
   return (
-    <Stack sx={{ maxWidth: "100%", ...messageStyle }}>
+    <Stack sx={{ maxWidth: "70%", ...messageStyle }}>
       <Panel
         sx={{
+          textAlign: "justify",
           mb: spacing * 0.5,
           px: spacing,
           py: spacing * 0.5,
-          background: "#ddd",
           overflow: "scroll",
 
           ...panelStyle,
         }}
       >
-        <Typography>{message.content}</Typography>
+        <Typography
+          component="div"
+          variant="body1"
+          sx={{
+            "& p": { margin: 0 },
+          }}
+        >
+          <ReactMarkdown>{message.content}</ReactMarkdown>
+        </Typography>
       </Panel>
     </Stack>
   );

09c73c8d6441f0d7358899459f3f0a4ddf75337e
zzou702 <zzou702@aucklanduni.ac.nz>
2023-08-16 16:23:00 +1200

    Modified explanation GUI, added getFeecback endpoint in backend

--- frontend/src/pages/WorkspacePage/Inspector/Panels/ExplanationsPanel.tsx
+++ frontend/src/pages/WorkspacePage/Inspector/Panels/ExplanationsPanel.tsx
@@ -55,6 +55,7 @@ export default function ExplanationsPanel() {
               {highlightedLines[0].split("\n").map((line, index) => (
                 <React.Fragment key={index}>
                   {line}
+
                   <br />
                 </React.Fragment>
               ))}
@@ -83,7 +84,7 @@ export default function ExplanationsPanel() {
             </Box>
           </Grid>
           <Grid item xs={12}>
-            <Box sx={{ display: "flex", justifyContent: "left", mx: 4 }}>
+            <Box sx={{ display: "flex", justifyContent: "left", mx: 4, mb: 2 }}>
               <Typography
                 variant="body1"
                 align="left"
@@ -94,7 +95,7 @@ export default function ExplanationsPanel() {
             </Box>
           </Grid>
           <Grid item xs={12}>
-            <Box sx={{ display: "flex", justifyContent: "left", mx: 4 }}>
+            <Box sx={{ display: "flex", justifyContent: "left", mx: 4, mb: 4 }}>
               <Typography variant="body1" align="left">
                 {explanation
                   ? explanation.split("\n").map((line, index) => (
@@ -109,60 +110,6 @@ export default function ExplanationsPanel() {
           </Grid>
         </>
       ) : (
-        // TODO: remove and replace this with null when backend is ready
-        // <>
-        //   <Grid item xs={12}>
-        //     <Box sx={{ mx: 4, my: 2 }}>
-        //       <Divider />
-        //     </Box>
-        //   </Grid>
-        //   <Grid item xs={12}>
-        //     <Box sx={{ display: "flex", justifyContent: "left", mx: 4 }}>
-        //       <Typography
-        //         variant="body1"
-        //         align="left"
-        //         sx={{ fontWeight: "bold" }}
-        //       >
-        //         Explanation:
-        //       </Typography>
-        //     </Box>
-        //   </Grid>
-        //   <Grid item xs={12}>
-        //     <Box sx={{ display: "flex", justifyContent: "left", mx: 4 }}>
-        //       <Typography
-        //         variant="body1"
-        //         align="left"
-        //         justifyContent="flex-end"
-        //       >
-        //         Certainly! The given line of code is a simple addition operation
-        //         within a JavaScript function. <br />
-        //         <br />
-        //         Let's break it down step by step in the context of the provided
-        //         program: <br />
-        //         <br />
-        //         The program defines a function called add that takes two
-        //         parameters, a and b. This means that when you call the add
-        //         function, you need to provide two values that will be assigned
-        //         to the variables a and b respectively. <br />
-        //         <br />
-        //         Inside the function, the line return a + b; is used. This line
-        //         performs the addition operation using the + operator between the
-        //         variables a and b. The + operator is used for addition when
-        //         applied to numbers. <br />
-        //         <br />
-        //         The return statement is used to specify the value that will be
-        //         returned by the function. In this case, the result of the
-        //         addition operation a + b is returned as the output of the add
-        //         function. <br />
-        //         <br />
-        //         To summarize, the add function takes two parameters a and b,
-        //         adds them together using the + operator, and returns the result.
-        //         This means that if you call the add function with, for example,
-        //         add(3, 4), it will return 7, because 3 + 4 equals 7.
-        //       </Typography>
-        //     </Box>
-        //   </Grid>
-        // </>
         <Grid item xs={12}>
           <CircularProgress />
         </Grid>
